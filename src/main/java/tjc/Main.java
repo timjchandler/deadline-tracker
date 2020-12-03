48d44bb709e4a0677160c0cc3cd9f2af53272de3
Tim <34661586+timjchandler@users.noreply.github.com>
2020-12-04 00:26:41 +0100

    basic gui home screen built

--- src/main/java/tjc/Main.java
+++ src/main/java/tjc/Main.java
@@ -4,9 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.SVGPath;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import tjc.controller.Controller;
+
+import javax.swing.*;
+import java.net.URL;
 
 public class Main extends Application {
 
@@ -16,13 +21,29 @@ public class Main extends Application {
         primaryStage.setTitle("Deadline Tracker");
         primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setScene(new Scene(root, 900, 600));
+        Scene scene = new Scene(root, 900, 600);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
 //        new Controller(primaryStage);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        try {
+            URL iconURL = Main.class.getResource("/media/logo.png");
+            java.awt.Image image = new ImageIcon(iconURL).getImage();
+            com.apple.eawt.Application.getApplication().setDockIconImage(image);
+        } catch (Exception e) {
+            // Sets task bar icon and name on mac, not needed for Windows or Linux
+        }
         launch(args);
     }
+
+    private void buildLogo() {
+
+    }
 }
+
+
+
--- src/main/java/tjc/controller/Controller.java
+++ src/main/java/tjc/controller/Controller.java
@@ -3,33 +3,47 @@ package tjc.controller;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Controller {
 
-//    private static Stage primaryStage;
-//
+    private double xOffset;
+    private double yOffset;
+
     @FXML
     private AnchorPane mainPane;
 
     @FXML
-    private Button closeBtn;
-
-    @FXML
     private Button minBtn;
-//
-//    public Controller(Stage primaryStage) {
-//        Controller.primaryStage = primaryStage;
-//    }
-//
+
     public void exit() {
         Platform.exit();
-        System.out.println("[EXIT] ---------------------------\n[EXIT] Shutting down");
         System.exit(0);
     }
 
     public void minimize() {
+        Stage stage = (Stage) minBtn.getScene().getWindow();
+        stage.setIconified(true);
+    }
 
+    public void panePressed(MouseEvent event) {
+        xOffset = minBtn.getScene().getWindow().getX() - event.getScreenX();
+        yOffset = minBtn.getScene().getWindow().getY() - event.getScreenY();
+    }
+
+    public void paneDragged(MouseEvent event) {
+        minBtn.getScene().getWindow().setX(event.getScreenX() + xOffset);
+        minBtn.getScene().getWindow().setY(event.getScreenY() + yOffset);
+    }
+
+    public void toggleMenu() {
+
+    }
+
+    protected void setOpacity(Boolean opaque) {
+        if (opaque) mainPane.setStyle("-fx-background-color: linear-gradient(to top right, rgba(2, 48, 71, 1), rgba(42, 157, 143, 1)");
+        else mainPane.setStyle("-fx-background-color: linear-gradient(to top right, rgba(2,48,71,0.8), rgba(42, 157, 143, 0.8)");
     }
 }
